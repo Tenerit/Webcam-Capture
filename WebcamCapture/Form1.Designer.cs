3174b8fafec06b5d16b327872800f550d5062bc3
Anthony <anthony.scottodaniello@ynov.com>
2020-07-15 14:58:04 +0200

    Update

---
+++ WebcamCapture/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WebcamCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.saveImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.takePictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveImagesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(907, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // saveImagesToolStripMenuItem
+            // 
+            this.saveImagesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.takePictureToolStripMenuItem});
+            this.saveImagesToolStripMenuItem.Name = "saveImagesToolStripMenuItem";
+            this.saveImagesToolStripMenuItem.Size = new System.Drawing.Size(106, 24);
+            this.saveImagesToolStripMenuItem.Text = "Save Images";
+            // 
+            // takePictureToolStripMenuItem
+            // 
+            this.takePictureToolStripMenuItem.Name = "takePictureToolStripMenuItem";
+            this.takePictureToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.takePictureToolStripMenuItem.Text = "Take Picture";
+            this.takePictureToolStripMenuItem.Click += new System.EventHandler(this.takePictureToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 511);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem saveImagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem takePictureToolStripMenuItem;
+    }
+}
+
---
+++ WebcamCapture/TakePicture.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarrenLee.Media;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace WebcamCapture
+{
+    public partial class TakePicture : Form
+    {
+        Camera myCamera = new Camera();
+
+        public TakePicture()
+        {
+            InitializeComponent();
+
+            GetInfo();
+            myCamera.OnFrameArrived += myCamera_OnFrameArrived;
+        }
+
+        private void myCamera_OnFrameArrived(object source, FrameArrivedEventArgs e)
+        {
+            Image img = e.GetFrame();
+            pictureBox1.Image = img;
+        }
+
+        private void GetInfo()
+        {
+            var cameraDevices = myCamera.GetCameraSources();
+            var cameraResolution = myCamera.GetSupportedResolutions();
+
+            foreach(var d in cameraDevices)
+            {
+                comboBox1.Items.Add(d);
+            }
+            foreach (var r in cameraResolution)
+            {
+                comboBox2.Items.Add(r);
+            }
+
+            comboBox2.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.ChangeCamera(comboBox1.SelectedIndex);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.Start(comboBox2.SelectedIndex);
+        }
+
+        private void TakePicture_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myCamera.Stop();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(@"D:\db\images"))
+                {
+                    Directory.CreateDirectory(@"D:\db\images");
+                    MessageBox.Show("Image Folder Created ...");
+                    string path = @"D:\db\images";
+
+                    pictureBox1.Image.Save(path + @"\" + textBox1.Text + ".Jpg", ImageFormat.Jpeg);
+                    MessageBox.Show("Image Saved ...");
+
+
+                }
+                else
+                {
+                    string path = @"D:\db\images";
+                    pictureBox1.Image.Save(path + @"\" + textBox1.Text + ".Jpg", ImageFormat.Jpeg);
+                    MessageBox.Show("Image Saved ...");
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+    }
+}
